69f67ea04f85a2caa99aa00f6d5870bd6003222d
Liu Yufan <142086067+TsLouis@users.noreply.github.com>
2026-09-16 20:38:27 +0800

    feat: add edge structural disconnect section

---
+++ frontend/src/lib/EdgeAuthoringSection.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import ObjectStructuralActions from './ObjectStructuralActions.svelte'
+  import { objectAction } from './object-authoring'
+  import type { EdgeDTO } from './types'
+
+  export let edge: EdgeDTO
+  export let onDisconnect: () => void = () => {}
+
+  $: disconnectAction = objectAction(edge.affordances, 'edge.delete')
+</script>
+
+{#if disconnectAction}
+  <ObjectStructuralActions {disconnectAction} {onDisconnect} />
+{/if}
